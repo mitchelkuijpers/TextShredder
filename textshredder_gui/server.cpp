9839c6b121d7c039f100a7456fa889d02b3b0fcb
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2011-04-26 15:58:23 +0200

    created a getpeer adress function  to resolve maken a temporary socket.

--- textshredder_gui/server.cpp
+++ textshredder_gui/server.cpp
@@ -1,6 +1,7 @@
 #include "server.h"
 #include "../textshredder_synchronization_lib/syncthread.h"
 #include "../textshredder_synchronization_lib/workingcopy.h"
+#include "../textshredder_synchronization_lib/textshredderconnection.h"
 
 Server::Server(QObject *parent):
     QTcpServer(parent)
@@ -9,18 +10,9 @@ Server::Server(QObject *parent):
 
 void Server::incomingConnection(int socketDescriptor)
 {
-	SyncThread *thread = new SyncThread(this, socketDescriptor,
-										*copy, true);
-	connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
-
-	QTcpSocket * tcp_sock_temp = new QTcpSocket();
-	tcp_sock_temp->setSocketDescriptor(socketDescriptor);
-
+	TextShredderConnection connection(this, socketDescriptor);
 	SyncableFile * syncfile = FileManager::Instance()->getFirstSyncableFileFromFileList();
-	QString ipAddr = tcp_sock_temp->peerAddress().toString();
-	syncfile->addClientWithName( ipAddr  );
-
-	thread->start();
+	syncfile->addClientWithName(connection.getPeerAdress());
 }
 
 bool Server::listenWithFile(const QHostAddress &address, quint16 port, QByteArray * fileContent)
