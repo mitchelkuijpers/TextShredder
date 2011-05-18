e1fce8d936e8f2807cc079a4b217f5257fbe1ee7
Maik Diepenbroek <m.diepenbroek@gmail.com>
2011-05-18 16:43:22 +0200

    changed config manager

--- textshredder_core/libraries/configuration/configurationmanager.h
+++ textshredder_core/libraries/configuration/configurationmanager.h
@@ -7,7 +7,7 @@
 #include "../network/textshredderconnection.h"
 #include <QDataStream>
 
-#define kDefaultConfigFile "/Users/maikdiepenbroek/MPNA/TextShredder/default_config.txt"
+#define kDefaultConfigFile "default_config.txt"
 
 class ConfigurationManager
 {
