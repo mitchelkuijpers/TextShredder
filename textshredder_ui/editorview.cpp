a9e09d53d03dfb462fc11403c7826dbcef3329d8
Wouter Konecny <wouterkonecny@gmail.com>
2011-05-18 11:26:26 +0200

    File TreeView now gets rebuild using the FileManager, instead of adding files from filenames

--- textshredder_ui/editorview.cpp
+++ textshredder_ui/editorview.cpp
@@ -71,31 +71,25 @@ void EditorView::addFolderToFileTreeWidget( QString directoryPath )
 
 void EditorView::rebuildSharedFilesListTreeView()
 {
+	qDebug("Rebuilding file list.");
 
+	QList < QSharedPointer<SyncableFile> > sharedFilesList =
+			FileManager::Instance()->getAllFiles();
 
-//	QDir dir;
-//	QStringList filters("*.txt");
-//	QStringList list = dir.entryList(filters);
+	int i = 0;
+	for(i = 0; i < sharedFilesList.count(); i++ ) {
+		QString fileName = sharedFilesList.at(i).data()->getFileAlias();
 
-//	int rowCount = model.rowCount();
+		QStandardItem *item = new QStandardItem( fileName );
+		item->setEditable( false );
+		item->setCheckable( true );
+		model.setItem(i, 0, item);
 
-//	int i = 0;
-//	for(i = 0; i < list.count(); i++ ) {
-//		QString filePath = list.at(i);
-//		FileManager::Instance()->addFileWithPath(filePath);
+		QStandardItem *status = new QStandardItem( QString("Not Syncing") );
+		model.setItem(i, 1, status);
+	}
 
-//		QStandardItem *item = new QStandardItem( filePath );
-//		item->setEditable( false );
-//		item->setCheckable( true );
-//		model.setItem(rowCount, 0, item);
-
-//		QStandardItem *status = new QStandardItem( QString("NS") );
-//		model.setItem(rowCount, 1, status);
-
-//		rowCount++;
-//	}
-
-//	ui->fileTreeWidget->setModel(&model);
+	ui->fileTreeWidget->setModel(&model);
 }
 
 
