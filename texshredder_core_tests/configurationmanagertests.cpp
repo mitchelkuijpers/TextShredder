4a0b4445fcb12665570c5efe617d0ca5417a8dd5
Maik Diepenbroek <m.diepenbroek@gmail.com>
2011-05-24 13:48:43 +0200

    Finally got 1 half ass test done

--- texshredder_core_tests/configurationmanagertests.cpp
+++ texshredder_core_tests/configurationmanagertests.cpp
@@ -2,5 +2,20 @@
 
 void ConfigurationManagerTests::testSetNewOptions()
 {
+	ConfigurationManager::Instance()->load();
+	ConfigurationOptions configOptions = ConfigurationManager::Instance()->getConfigurationOptions();
+	quint16 oldServerPort = configOptions.getServerPort();
+	QString oldServerIp = configOptions.getLastKnownIp();
+
+	QString newIp = "2939.33.3.3";
+	configOptions.setServerPort((quint16) 3939);
+	configOptions.setLastKnownIp(newIp);
+
+	ConfigurationManager::Instance()->save();
+
+	QVERIFY2(oldServerPort != configOptions.getServerPort(), "The serverport has not changed");
+	QVERIFY2(oldServerIp != configOptions.getLastKnownIp(), "The ip has not changed");
 
 }
+
+
