882d34fecfd55533ead03ef886c7c10f79bb81d3
stil4m <mstijlaart@gmail.com>
2011-05-25 13:49:27 +0200

    Commented the backup copy, Edit and EditList

--- textshredder_core/libraries/synchronization/models/edit.h
+++ textshredder_core/libraries/synchronization/models/edit.h
@@ -8,17 +8,20 @@ class Edit : public QObject
 {
     Q_OBJECT
 public:
-    /**
-      * Create new Edit without parameters
-      */
-    Edit(QObject *parent);
+	/**
+	  * Constructor which creates  a new Edit without parameters.
+	  *
+	  * @param parent The parent object.
+	  */
+	Edit( QObject *parent );
 
 	/**
-	  * Create new Edit with a localVersion and patches given
-	  * @param localVersion of the Edit
-	  * @param patches of the Edit
+	  * Constructor which creates  new Edit with a localVersion and given patches.
+	  *
+	  * @param localVersion The localVersion of the Edit.
+	  * @param patches The patches the Edit will hold.
 	  */
-	Edit(QObject *parent, unsigned int localVersion, QList<Patch> & patches);
+	Edit( QObject *parent, unsigned int localVersion, QList<Patch> & patches );
 
 	/**
 	  * Constructor to make Edit from bytes. Normally used to send an edit
@@ -26,45 +29,81 @@ public:
 	  *
 	  * @param the bytes used to make the edit.
 	  */
-	Edit(QObject *, QByteArray &);
+	Edit( QObject *parent, QByteArray & byteRepresentation );
 
 	/**
 	  * Copy constructor.
+	  *
+	  * @param copy The edit which will be copied. The local version and the edits will be copied.
 	  */
-	Edit(const Edit &copy);
+	Edit( const Edit &copy );
 
 	/**
-	  * Overloaded the assign operator.
+	  * Operator overload for the = operator.
+	  * The local version and the edits will be assigned.
+	  *
+	  * @param other The object to which this Edit object gets assigned to.
+	  *
+	  * @return A reference to this object that has been assigned to the other parameter.
 	  */
-	Edit& operator = (const Edit&);
+	Edit & operator=( const Edit & other );
 
 	/**
-	  * Overloaded the equals operator.
+	  * Operator overload for the == operator.
+	  * The Edit object will be compared to the other Edit object.
+	  * The comparison will be on the localVersion and on the list off patches.
+	  *
+	  * @param other The object this object is being compared to.
+	  *
+	  * @return A boolean value if both object are equal. Returns true if so.
 	  */
-	bool operator == (const Edit&);
+	bool operator==( const Edit & other );
 
 	/**
-	  * Will append the string reprentation to a QByteArray
+	  * Method will append the byte representation of this Edit object to the QByteArray passed as parameter.
+	  *
+	  * @param bytesArray The QByteArray in which this Edit object is stored.
 	  */
 	void appendEditBytesToByteArray(QByteArray &bytesArray);
 
-	//Setters
+	/**
+	  * Method will set the local version of this Edit to a new local version.
+	  *
+	  * @param newLocalVerion The new local version for the Edit.
+	  */
 	void setLocalVersion(unsigned int newLocalVersion);
+
+	/**
+	  * Method will set the list of patches for the Edit object to the given parameter.
+	  *
+	  * @param newLocalVerion The new local version for the Edit.
+	  */
 	void setPatches(QList<Patch> & newPatches);
 
-	//Getters
+	/**
+	  * Method will return the local version of this Edit object.
+	  *
+	  * @return The current local version.
+	  */
 	unsigned int getLocalVersion();
+
+	/**
+	  * Method will return the patches stored in the Edit object.
+	  *
+	  * @return The current patches stored in the edit.
+	  */
 	QList<Patch> & getPatches();
 
-
-signals:
-
-public slots:
-
 private:
-    unsigned int localVersion;
-	QList<Patch> patches;
+	/**
+	  * The local version of the Edit object.
+	  */
+	unsigned int localVersion;
 
+	/**
+	  * The list of patches stored in the Edit object.
+	  */
+	QList<Patch> patches;
 
 };
 
--- textshredder_core/libraries/synchronization/models/editlist.h
+++ textshredder_core/libraries/synchronization/models/editlist.h
@@ -9,75 +9,129 @@
 
 class EditList : public QObject, public QMutex
 {
-    Q_OBJECT
+	Q_OBJECT
 public:
 	/**
-	  * Constructor which makes an EditList with or withou a remote version.
+	  * Constructor which makes an EditList with or without a remote version.
 	  *
-	  * @param the remoteVersion which will be used.
+	  * @param parent The parent object.
+	  * @param remoteVersion The remoteVersion which will be used. Default is 0.
 	  */
-    EditList(QObject *parent, int remoteVersion = 0);
+	EditList( QObject *parent, int remoteVersion = 0 );
 
 	/**
-	  * Constructor which makes an EditList with QByteArray
+	  * Constructor which makes an EditList with QByteArray.
+	  * This byte array should be conform to the byte representation of an EditList object.
 	  *
-	  * @param the remoteVersion which will be used.
+	  * @param parent The parent object.
+	  * @param content The content on which the new EditList is based.
 	  */
 	EditList(QObject *parent, QByteArray &content);
+
 	/**
-	  * Constructor which makes an EditList with a TextShredderPacket object
+	  * Constructor which makes an EditList with a TextShredderPacket object.
+	  * The content of this packet will be used for constructing the packet.
 	  *
-	  * @param the remoteVersion which will be used.
+	  * @param parent The parent object.
+	  * @param packet The packet on which this the EditList object is based on.
 	  */
-	EditList(QObject *parent, TextShredderPacket &);
-
-	EditList(const EditList &);
-	EditList & operator=(EditList &);
-	bool operator==(EditList &);
+	EditList(QObject *parent, TextShredderPacket &packet);
 
 	/**
-	  * Check whetter EditList does not contain any Edit objects.
+	  * Copy constructor.
+	  * The list of Edits and the remoteVersion will be copied.
+	  *
+	  * @param other The EditList object which will be copied.
+	  */
+	EditList( const EditList & other);
+
+	/**
+	  * Operator overload for the = operator.
+	  * The list of Edits and the remoteVersion will be assigned.
+	  *
+	  * @param other The EditList object which will be assigned.
+	  *
+	  * @return A reference to this object after the assignment.
+	  */
+	EditList & operator=(EditList & other );
+
+	/**
+	  * Operator overload for the == operator.
+	  * Will compare the EditList objects on their remote version and lists of Edit objects.
+	  *
+	  * @param other The object which this EditList object is compared to.
+	  *
+	  * @return The result of the comparison. True if both EditList objects are equal.
+	  */
+	bool operator==( EditList & other );
+
+	/**
+	  * Method to check whetter the EditList does not contain any Edit objects.
+	  *
+	  * @return Returns true if there are no Edit object in the edits variable.
 	  */
 	bool isEmpty();
 
 	/**
-	  * Empty the edit list.
+	  * Method will remove all the Edit objects from the edits variable.
 	  */
 	void empty();
 
 	/**
-	  * Function will add an Edit object to the edits in the EditList object
-	  */
-	void addEdit( const Edit & );
-
-	/**
-	  * Function will update EditList to a given remote version.
+	  * Function will add an Edit object to the edits in the EditList object.
 	  *
-	  * @param the remote version to update to.
+	  * @param edit The Edit object that should be added to the list.
 	  */
-	void updateToRemote(	unsigned int newRemoteVersion);
+	void addEdit( const Edit & edit );
 
 	/**
-	  * Function will return a packet with the EditList as content.
+	  * Method will return a allocated packet based on the byte representation of the EditList object.
+	  *
+	  * @return The allocated TextShredderPacket pointer.
 	  */
 	TextShredderPacket * getAllocatedPacket();
 
-	//Setters
-	void setRemoteVersion(unsigned int);
-
-	//Getters
-	unsigned int getRemoteVersion();
-	QList<Edit> & getEdits();
+	/**
+	  * Method will remove the edits up to a local version from the edits variable.
+	  * These popped edits are put into a variable and returned by the function.
+	  * If 5 is passed as variable. Edits with version 4 and below are popped.
+	  *
+	  * @param version The version till which the Edit objects should be popped.
+	  *
+	  * @return The list with popped Edit objects.
+	  */
 	QList<Edit> popEditsUpToLocalVersion(unsigned int version);
+
+	/**
+	  * Setter for the remoteVersion variable.
+	  *
+	  * @param newRemoteVersion The new remote version for the EditList object.
+	  */
+	void setRemoteVersion( unsigned int newRemoteVersion);
+
+	/**
+	  * Getter for the remoteVersion variable.
+	  *
+	  * @return The current remote version of the EditList object.
+	  */
+	unsigned int getRemoteVersion();
+
+	/**
+	  * Getter for the edits variable.
+	  *
+	  * @return The edits variable.
+	  */
+	QList<Edit> & getEdits();
+
 private:
+	/** The list of edits. */
 	QList<Edit> edits;
-    int remoteVersion;
-    QMutex mutex;
 
-signals:
-
-public slots:
-
+	/**
+	  * The remoteVersion of the object. Is used as an acknowledgement.
+	  * This is the version on which edits are based on.
+	  */
+	int remoteVersion;
 };
 
 #endif // EDITLIST_H
