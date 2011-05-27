d4d36e34fce04d4fb12258bc64c1c018ab1dae09
Mats Stijlaart <mstijlaart@gmail.com>
2011-05-27 11:32:20 +0200

    Commented the working copy and the ClientRepresentation #10

--- textshredder_core/libraries/synchronization/clientrepresentation.h
+++ textshredder_core/libraries/synchronization/clientrepresentation.h
@@ -3,45 +3,85 @@
 
 #include <QObject>
 #include <QString>
+
 #include "../network/textshredderconnection.h"
-#include "./syncthread.h"
-#include "./filesync.h"
+#include "syncthread.h"
+#include "filesync.h"
 
 class ClientRepresentation : public QObject
 {
     Q_OBJECT
 public:
 	/**
-	 * Constructor. Opens up a new connection on the socket
+	 * Constructor. Opens up a new connection on the socket descriptor.
 	 * Gets the file to be synced and actually sync it.
 	 * Add the client name to the client list
 	 *
-	 * @param Qobject parent.
-	 * @param int The socketdescriptor
+	 * @param parent The Parent object parent.
+	 * @param socketDescriptor The socketdescriptor on which the connection is opened.
 	 */
 	ClientRepresentation(QObject *parent, int socketDescriptor);
 
+	/**
+	  * Method will return the current alias set by the client which the connection is connected to.
+	  *
+	  * @return The current alias.
+	  */
 	QString getAlias();
+
+	/**
+	  * Method will send an packet on the connection.
+	  *
+	  * @param packet The packet that should be send.
+	  */
 	void sendPacket(TextShredderPacket &packet);
+
 private:
+	/**
+	  * Method will setup all the signals needed.
+	  */
+	void setupSignals();
+
+	/**
+	  * The connection on which this ClientRepresentation instance communicates.
+	  */
 	QSharedPointer<TextShredderConnection> connection;
+
+	/**
+	  * THe current set alias by the connected client.
+	  */
 	QString alias;
 
 private slots:
 	/**
-	 * Removes the disconnected clients from the list
+	 * Will disconnect the signals from the connection, remove the alias from the user manager,
+	 * and emit the signal clientRepresentationEncounteredEnd()
 	 */
 	void getDisconnected();
+
+	/**
+	  * Will handle a request from a file. This slot is connected to receivedFileRequest from the TextShredderConnextion.
+	  *
+	  * @param packet The packet containing the file request.
+	  */
 	void handleFileRequest(TextShredderPacket& packet);
 
 	/**
-	 * ?? NOT CLEAR
-	 * @param QByteArray bytes
+	 * Will update the alias in the UserManager based on the bytes received in the SetAliasPacket.
+	 *
+	 * @param QByteArray bytes The content of the SetAliasPacket.
 	 */
 	void processSetAliasPacketContent(QByteArray &bytes);
 
 signals:
+	/**
+	  * Signal that will be emitted when the connection ended.
+	  */
 	void clientRepresentationEncounteredEnd();
+
+	/**
+	  * Signal that will be emitted when the alias of the representation did change.
+	  */
 	void clientRepresentationDidChangeAlias();
 };
 
