741658ddc46d6d4e80ec667918e1e5e4a39e24c0
Mats Stijlaart <mstijlaart@gmail.com>
2011-05-30 12:14:32 +0200

    #56 Added class commenting

--- textshredder_core/libraries/network/models/editlistpacket.h
+++ textshredder_core/libraries/network/models/editlistpacket.h
@@ -7,13 +7,46 @@
 class EditListPacket : public TextShredderPacket
 {
 public:
+	/**
+	  * Constructor to make a packet with its content representing an EditList.
+	  *
+	  * @param parent The parent object.
+	  * @param editList The EditList which will be used to make the content.
+	  */
 	EditListPacket(QObject *parent, EditList &editList);
 
+	/**
+	  * Static method to create an EditList object from packet content.
+	  *
+	  * @param packetContent The bytes representing the EditList.
+	  *
+	  * @return The new EditList instance.
+	  */
 	static EditList * GetAllocatedEditListFromPacketContent(QByteArray packetContent);
+
 private:
+	/**
+	  * Method will fill an byteArray with an EditList instance.
+	  *
+	  * @param packetContent The array in which the byte representation should be stored in.
+	  * @param editList The EditList which has to be serialized.
+	  */
 	void fillByteArrayWithEditList(QByteArray &packetContent, EditList &editList);
+
+	/**
+	  * Method will fill an byteArray with an Edit instance.
+	  *
+	  * @param packetContent The array in which the byte representation should be stored in.
+	  * @param editList The Edit which has to be serialized.
+	  */
 	void appendEditBytesToByteArray(QByteArray &byteArray, Edit edit);
 
+	/**
+	  * Method will setup a Edit with bytes.
+	  *
+	  * @param edit The Edit that has to be initialized.
+	  * @param bytes The bytes used for the Edit initialization.
+	  */
 	static void setupEditWithBytes(Edit &edit, QByteArray &bytes);
 };
 
