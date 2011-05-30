f9ebff9ef95bab2e015e58f0f447682c684af909
Maik Diepenbroek <m.diepenbroek@gmail.com>
2011-05-30 10:48:40 +0200

    Updated the class commenting, on 90% now

--- textshredder_core/libraries/notification/models/notificationoption.h
+++ textshredder_core/libraries/notification/models/notificationoption.h
@@ -7,10 +7,32 @@ class NotificationOption : public QObject
 {
     Q_OBJECT
 public:
+	/**
+	 * The copy constructor
+	 * @param other A constant NotificationOption object
+	 */
 	NotificationOption(const NotificationOption &other);
+	/**
+	 * The constructor
+	 * @param parent Qobject pointer to access the methods
+	 * @param label The label variable is set.
+	 */
 	NotificationOption(QObject *parent, const QString label);
+	/**
+	 * The constructor
+	 * @param parent Qobject pointer to access the methods
+	 * @param label The label variable is set.
+	 * @param function A void pointer to a passed function.
+	 */
 	NotificationOption(QObject *parent, const QString label, void * function);
+	/**
+	 * Get the notification label
+	 */
 	QString getLabel();
+	/**
+	 * The equals operator is overloaded to set the notificationOption
+	 * @param NotificationOption& The Notification option to set
+	 */
 	NotificationOption & operator =(const NotificationOption &);
 
 private:
